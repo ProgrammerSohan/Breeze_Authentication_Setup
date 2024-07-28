e1a51c3a9e908dd5ec570c1d889dd2badb67aaa5
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-28 16:58:08 +0600

    email verify in laravel

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');//email verified middleware
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
